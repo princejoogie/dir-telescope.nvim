14a25ea1aeb73c00434c61c429cb147b2f65d405
nade4tt <denisnarat8@gmail.com>
2025-06-03 22:27:16 +0200

    fix(lua): update vim.validate usage to new syntax

--- lua/dir-telescope/settings.lua
+++ lua/dir-telescope/settings.lua
@@ -15,13 +15,11 @@ M.current = M._DEFAULT_SETTINGS
 
 M.set = function(opts)
 	M.current = vim.tbl_deep_extend("force", M.current, opts)
-	vim.validate({
-		hidden = { M.current.hidden, "boolean" },
-		debug = { M.current.debug, "boolean" },
-		no_ignore = { M.current.no_ignore, "boolean" },
-		show_preview = { M.current.show_preview, "boolean" },
-		follow_symlinks = { M.current.follow_symlinks, "boolean" },
-	})
+	vim.validate("hidden", M.current.hidden, "boolean")
+	vim.validate("debug", M.current.debug, "boolean")
+	vim.validate("no_ignore", M.current.no_ignore, "boolean")
+	vim.validate("show_preview", M.current.show_preview, "boolean")
+	vim.validate("follow_symlinks", M.current.follow_symlinks, "boolean")
 end
 
 return M
